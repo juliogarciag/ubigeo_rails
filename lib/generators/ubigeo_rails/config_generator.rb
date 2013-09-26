0f6feea58c5525d6783fd21abf956f30febb8d7d
Jgarcia <jgarcia@phantasia.pe>
2013-09-26 12:40:47 -0500

    ...

--- lib/generators/ubigeo_rails/config_generator.rb
+++ lib/generators/ubigeo_rails/config_generator.rb
@@ -1,6 +1,6 @@
 module UbigeoRails
   module Generators
-    class ConfigGenerator < Rails::Generators::Base
+    class ConfigGenerator < ::Rails::Generators::Base
       def create_config_file
         create_file "config/ubigeo_rails.rb", "Hola :D"
       end
