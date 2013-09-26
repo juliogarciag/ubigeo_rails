2b7730bed0eb9252dcaa3f0f9aa8e33eec47bffe
Jgarcia <jgarcia@phantasia.pe>
2013-09-26 13:03:04 -0500

    configurable

--- lib/generators/ubigeo_rails/config_generator.rb
+++ lib/generators/ubigeo_rails/config_generator.rb
@@ -2,12 +2,18 @@ module UbigeoRails
   module Generators
     class ConfigGenerator < ::Rails::Generators::Base
       def create_config_file
-        create_file "config/ubigeo_rails.rb", <<CODE
-          UbigeoRails.config do |config|
-            config.table_name 'ubigeo_ubigeo'
-            config.db_connection { Rails.env }
-          end
-        CODE
+        create_file "config/ubigeo_rails.rb", config_file_content
+      end
+      
+      private
+      
+      def config_file_content
+<<CODE
+UbigeoRails.config do |config|
+  config.table_name 'ubigeo_ubigeo'
+  config.db_connection { Rails.env }
+end
+CODE
       end
     end
   end
